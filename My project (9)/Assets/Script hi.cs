2f3d280412ecd945f910b00f228c9dd64e28154e
2026137050 <eostmfhektzja4143@gmail.com>
2026-03-12 15:43:22 +0900

    유니티 파일 변경

---
+++ My project (9)/Assets/Script hi.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("안녕하세요?");
+        Debug.LogError("안녕하세요?22");
+        Debug.LogWarning("안녕하세요?333");
+    }
+
+    void Update()
+    {
+        
+    }
+    
+}
---
+++ My project (9)/Assets/script.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    public string playerName;
+
+
+    private void Start()
+    {
+        Debug.Log
+    }
+}
