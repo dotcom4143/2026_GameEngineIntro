6520dc991abbce4f6e2c67723d570e19d0fe64b8
dotcom4143 <eostmfhektzja4143>
2026-03-26 17:05:58 +0900

    돌아가기 버튼까지 생성

---
+++ 게임엔진입문 프로젝트 이주원/Assets/Script/title manager1.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class titlemanager1 : MonoBehaviour
+{
+    public void GameStart()
+    {
+        SceneManager.LoadScene("UI");
+    }
+}
